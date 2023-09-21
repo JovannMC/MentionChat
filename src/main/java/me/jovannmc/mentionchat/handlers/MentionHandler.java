a9efe54fc67126555f9bd5476f80d7642ad16845
JovannMC <45893380+JovannMC@users.noreply.github.com>
2023-09-21 19:11:23 +0300

    Remove MESSAGE mention type

--- src/main/java/me/jovannmc/mentionchat/handlers/MentionHandler.java
+++ src/main/java/me/jovannmc/mentionchat/handlers/MentionHandler.java
@@ -89,66 +89,43 @@ public class MentionHandler implements Listener {
 
         // Remove all recipients to send custom messages to each player, but lets the message still be logged in the console
         e.getRecipients().removeAll(Bukkit.getOnlinePlayers());
-        String type = getConfig().getString("mentionType");
 
         // We use a HashSet here to track which players have already been sent a message, to prevent duplicate messages
         HashSet<Player> sentMessages = new HashSet<>();
 
-        if (type.equalsIgnoreCase("MESSAGE")) {
-            // Small bug here where with multiple mentions, the "mentionedMessage" may appear after the message instead of before.
-            // It's inconsistency caused by the loop but doesn't really need to be fixed.
-            for (Player mentionedPlayer : mentioned) {
-                Utils.sendMessage(mentionedPlayer, getConfig().getString("mentionedMessage").replace("%player%", mentioner.getName()));
-                playMentionSound(mentionedPlayer);
+        for (Player mentionedPlayer : mentioned) {
+            String mentionPattern = "(?i)@" + mentionedPlayer.getName() + "\\b";
+            String mentionMessage;
 
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    if (!sentMessages.contains(player)) {
-                        // Add the player to the HashSet, so they don't get sent the same message multiple times
-                        player.sendMessage(e.getFormat().replace("%1$s", mentioner.getDisplayName()).replace("%2$s", e.getMessage()));
-                        sentMessages.add(player);
-                    }
-                }
+            if (plugin.getData().contains(mentionedPlayer.getUniqueId().toString() + ".format")) {
+                mentionMessage = ChatColor.translateAlternateColorCodes('&', plugin.getData().getString(mentionedPlayer.getUniqueId().toString() + ".format").replace("%mention%", "@" + mentionedPlayer.getName()));
+            } else {
+                mentionMessage = ChatColor.translateAlternateColorCodes('&', getConfig().getString("mentionFormat").replace("%mention%", "@" + mentionedPlayer.getName()));
             }
-        } else if (type.equalsIgnoreCase("FORMAT")) {
-            for (Player mentionedPlayer : mentioned) {
-                String mentionPattern = "(?i)@" + mentionedPlayer.getName() + "\\b";
-                String mentionMessage;
 
-                if (plugin.getData().contains(mentionedPlayer.getUniqueId().toString() + ".format")) {
-                    mentionMessage = ChatColor.translateAlternateColorCodes('&', plugin.getData().getString(mentionedPlayer.getUniqueId().toString() + ".format").replace("%mention%", "@" + mentionedPlayer.getName()));
+            // Like previously, we split the message into words and check if any of them are a player's name to prevent duplicates
+            String[] words = e.getMessage().split("\\s+");
+            StringBuilder newMessageBuilder = new StringBuilder();
+            for (String word : words) {
+                if (word.matches(mentionPattern)) {
+                    newMessageBuilder.append(" ").append(mentionMessage);
                 } else {
-                    mentionMessage = ChatColor.translateAlternateColorCodes('&', getConfig().getString("mentionFormat").replace("%mention%", "@" + mentionedPlayer.getName()));
+                    newMessageBuilder.append(" ").append(word);
                 }
-
-                // Like previously, we split the message into words and check if any of them are a player's name to prevent duplicates
-                String[] words = e.getMessage().split("\\s+");
-                StringBuilder newMessageBuilder = new StringBuilder();
-                for (String word : words) {
-                    if (word.matches(mentionPattern)) {
-                        newMessageBuilder.append(" ").append(mentionMessage);
-                    } else {
-                        newMessageBuilder.append(" ").append(word);
-                    }
-                }
-
-                String newMessage = newMessageBuilder.toString().trim();
-
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    if (!mentioned.contains(player) && !sentMessages.contains(player)) {
-                        // Add the player to the HashSet, so they don't get sent the same message multiple times
-                        player.sendMessage(e.getFormat().replace("%1$s", mentioner.getDisplayName()).replace("%2$s", e.getMessage()));
-                        sentMessages.add(player);
-                    }
-                }
-
-                playMentionSound(mentionedPlayer);
-                mentionedPlayer.sendMessage(e.getFormat().replace("%1$s", mentioner.getDisplayName()).replace("%2$s", newMessage));
             }
+            String newMessage = newMessageBuilder.toString().trim();
 
-            nextMention.put(mentioner.getUniqueId(), System.currentTimeMillis());
-        } else {
-            Bukkit.getLogger().log(Level.SEVERE, "Invalid mention type in MentionChat's config. (" + type + ")");
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (!mentioned.contains(player) && !sentMessages.contains(player)) {
+                    // Add the player to the HashSet, so they don't get sent the same message multiple times
+                    player.sendMessage(e.getFormat().replace("%1$s", mentioner.getDisplayName()).replace("%2$s", e.getMessage()));
+                    sentMessages.add(player);
+                }
+            }
+            playMentionSound(mentionedPlayer);
+            mentionedPlayer.sendMessage(e.getFormat().replace("%1$s", mentioner.getDisplayName()).replace("%2$s", newMessage));
         }
+        nextMention.put(mentioner.getUniqueId(), System.currentTimeMillis());
     }
 
     private void mentionEveryone(AsyncPlayerChatEvent e, Player mentioner) {
@@ -166,33 +143,24 @@ public class MentionHandler implements Listener {
 
         // Remove all recipients to send custom messages to each player, but lets the message still be logged in the console
         e.getRecipients().removeAll(Bukkit.getOnlinePlayers());
-        String type = getConfig().getString("mentionType");
 
         for (Player p : Bukkit.getOnlinePlayers()) {
-            if (type.equalsIgnoreCase("MESSAGE")) {
-                Utils.sendMessage(p, getConfig().getString("mentionedMessage").replace("%player%", mentioner.getName()));
-                p.sendMessage(e.getFormat().replace("%1$s", mentioner.getDisplayName()).replace("%2$s", e.getMessage()));
-            } else if (type.equalsIgnoreCase("FORMAT")) {
-                String mentionPattern = "(?i)@everyone\\b";
-                String mentionMessage = ChatColor.translateAlternateColorCodes('&', getConfig().getString("mentionFormat").replace("%mention%", "@everyone"));
+            String mentionPattern = "(?i)@everyone\\b";
+            String mentionMessage = ChatColor.translateAlternateColorCodes('&', getConfig().getString("mentionFormat").replace("%mention%", "@everyone"));
 
-                // Like previously, we split the message into words so that it has to be @everyone, and also case-insensitive
-                String[] words = e.getMessage().split("\\s+");
-                StringBuilder newMessageBuilder = new StringBuilder();
-                for (String word : words) {
-                    if (word.matches(mentionPattern)) {
-                        newMessageBuilder.append(" ").append(mentionMessage);
-                    } else {
-                        newMessageBuilder.append(" ").append(word);
-                    }
+            // Like previously, we split the message into words so that it has to be @everyone, and also case-insensitive
+            String[] words = e.getMessage().split("\\s+");
+            StringBuilder newMessageBuilder = new StringBuilder();
+            for (String word : words) {
+                if (word.matches(mentionPattern)) {
+                    newMessageBuilder.append(" ").append(mentionMessage);
+                } else {
+                    newMessageBuilder.append(" ").append(word);
                 }
-
-                String newMessage = newMessageBuilder.toString().trim();
-
-                p.sendMessage(e.getFormat().replace("%1$s", mentioner.getDisplayName()).replace("%2$s", newMessage));
-            } else {
-                Bukkit.getLogger().log(Level.SEVERE, "Invalid mention type in MentionChat's config. (" + type + ")");
             }
+            String newMessage = newMessageBuilder.toString().trim();
+            p.sendMessage(e.getFormat().replace("%1$s", mentioner.getDisplayName()).replace("%2$s", newMessage));
+
             nextMention.put(mentioner.getUniqueId(), System.currentTimeMillis());
             playMentionSound(p);
         }
