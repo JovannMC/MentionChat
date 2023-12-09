47a4959fa83488a0ae4c3b3a4461d39d438e8b96
JovannMC <45893380+JovannMC@users.noreply.github.com>
2023-12-09 14:48:28 +0300

    Fix duplicated messages

--- src/main/java/me/jovannmc/mentionchat/handlers/MentionTypeMessageHandler.java
+++ src/main/java/me/jovannmc/mentionchat/handlers/MentionTypeMessageHandler.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class MentionTypeMessageHandler {
 
@@ -17,8 +18,9 @@ public class MentionTypeMessageHandler {
         FileConfiguration config = plugin.getConfig();
         FileConfiguration data = plugin.getData();
 
-        // Remove all recipients to send custom messages to each player, but lets the message still be logged in the console
-        e.getRecipients().removeAll(Bukkit.getOnlinePlayers());
+        // Create a new HashSet for recipients
+        // interacting with event's recipients causes issues for some reason, so we create a new hashset with the recipients
+        Set<Player> recipients = new HashSet<>(e.getRecipients());
 
         // We use a HashSet here to track which players have already been sent a message, to prevent duplicate messages
         HashSet<Player> sentMessages = new HashSet<>();
@@ -35,20 +37,16 @@ public class MentionTypeMessageHandler {
                 } else {
                     Utils.sendMessage(mentionedPlayer, config.getString("mentionedMessage").replace("%player%", mentioner.getName()));
                 }
+                System.out.println("send message to " + mentionedPlayer.getName());
 
-                if (plugin.getData().get(mentionedPlayer.getUniqueId().toString() + ".toggle.format") != null && !plugin.getData().getBoolean(mentionedPlayer.getUniqueId().toString() + ".toggle.format")) {
-                    mentionedPlayer.sendMessage(e.getFormat().replace("%1$s", mentioner.getDisplayName()).replace("%2$s", e.getMessage()));
-                    System.out.println("send message to " + mentionedPlayer.getName());
-                }
+                // Remove mentioned players from the recipients set
+                recipients.remove(mentionedPlayer);
             }
         }
 
-        // Send normal message to everyone else
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            if (!mentioned.contains(player) && plugin.getData().get(player.getUniqueId().toString() + ".toggle.format") != null && !plugin.getData().getBoolean(player.getUniqueId().toString() + ".toggle.format")) {
-                player.sendMessage(e.getFormat().replace("%1$s", mentioner.getDisplayName()).replace("%2$s", e.getMessage()));
-                System.out.println("send normal to " + player.getName());
-            }
+        for (Player player : recipients) {
+            player.sendMessage(e.getFormat().replace("%1$s", mentioner.getDisplayName()).replace("%2$s", e.getMessage()));
+            System.out.println("send normal to " + player.getName());
         }
     }
 
